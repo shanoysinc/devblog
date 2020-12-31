8fc49a8ec0c62012b18d7a7d12697dde78eccc1a
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-31 13:01:51 -0800

    remove axios fetch all data use swr instead

--- features/post/AsyncActions.ts
+++ features/post/AsyncActions.ts
@@ -2,11 +2,8 @@ import { fetchPost, addPost } from "./postSlice";
 import * as postApi from "../../api/post";
 import { Post } from "../../shared/interfaces/Post";
 
-export const fetchAllPost = () => async (dispatch) => {
-	const { data } = await postApi.fetchAllPost();
-	console.log("fecth data", data);
-
-	return dispatch(fetchPost(data.posts));
+export const fetchAllPost = (post) => async (dispatch) => {
+	return dispatch(fetchPost(post));
 };
 
 export const createPost = (newPost: Post) => async (dispatch) => {

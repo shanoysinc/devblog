c1ea2e4170711c6828afa55e760e3914ee835bfc
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-31 13:03:37 -0800

    redirect to homepage

--- components/quill/Quill.tsx
+++ components/quill/Quill.tsx
@@ -3,9 +3,10 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useAppDispatch } from "../../pages/_app";
 import { createPost } from "../../features/post/AsyncActions";
-
+import { useRouter } from "next/router";
 const Quill = () => {
 	const dispatch = useAppDispatch();
+	const router = useRouter();
 
 	const [content, setContent] = useState("");
 	const [tags, setTags] = useState("");
@@ -17,6 +18,7 @@ const Quill = () => {
 		setContent("");
 		setTags("");
 		setTitle("");
+		router.push("/");
 	};
 
 	const modules = {

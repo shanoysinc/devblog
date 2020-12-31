24e289f8b126db331c007690e39e656086ecd13a
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-31 13:01:20 -0800

    remove axios fetch all data use swr instead

--- api/post.ts
+++ api/post.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 import { Post } from "../shared/interfaces/Post";
 
 const url = "http://localhost:3000/api/blog";
-export const fetchAllPost = () => axios.get(url);
+
 export const createPost = (newPost: Post) => axios.post(url, newPost);

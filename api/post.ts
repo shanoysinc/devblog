ceafc4179fffb5f09821b4d499dce22a86ed0e73
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-31 09:56:49 -0800

    add Post type

--- api/post.ts
+++ api/post.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { Post } from "../shared/interfaces/Post";
 
 const url = "http://localhost:3000/api/blog";
 export const fetchAllPost = () => axios.get(url);
+export const createPost = (newPost: Post) => axios.post(url, newPost);

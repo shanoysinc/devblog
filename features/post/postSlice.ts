66f727304cf72b911c217046d838e40d200a1f4b
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-31 09:57:59 -0800

    remove old async code

--- features/post/postSlice.ts
+++ features/post/postSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import * as postApi from "../../api/post";
 
+// return Object  type from mongoose
 interface Post {
 	_id: string;
 	tags: string[];
@@ -9,30 +9,16 @@ interface Post {
 	__v: number;
 }
 
-// const fetchAllPost = createAsyncThunk("post/fetchAllPost", async () => {
-// 	const { data } = await postApi.fetchAllPost();
-// 	return data;
-// });
-
 const postSlice = createSlice({
 	name: "post",
 	initialState: [] as Post[],
 	reducers: {
 		fetchPost: (state, action: PayloadAction<Post[]>) => action.payload,
+		addPost: (state, action) => [action.payload, ...state],
 	},
-	// extraReducers: {
-	// 	[fetchAllPost.fulfilled]: (state, action) => action.payload,
-	// },
 });
 
 const { actions, reducer } = postSlice;
-const { fetchPost } = actions;
-
-export const fetchAllPost = () => async (dispatch) => {
-	const { data } = await postApi.fetchAllPost();
-	console.log("fecth data", data);
-
-	return dispatch(fetchPost(data.posts));
-};
+export const { fetchPost, addPost } = actions;
 
 export default reducer;

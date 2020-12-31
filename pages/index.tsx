9de1d4b6da74b0798b2bc54071c457660a4476ae
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-12-31 10:44:06 -0800

    add statue for currentPostId

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
-import Head from "next/head";
+import React, { useState } from "react";
 import styles from "../styles/Home.module.css";
 import PostContent from "../components/sidebar/PostContent";
 import Posts from "../components/sidebar/Posts";
 import Panel from "../components/sidebar/Panel";
 import NavBar from "../components/header/NavBar";
 export default function Home() {
+	const [currentPostId, setCurrentPostId] = useState("");
+
 	return (
 		<div>
 			<NavBar />
 			<div className={styles.container}>
 				<Panel />
-				<Posts />
-				<PostContent />
+				<Posts setCurrentPostId={setCurrentPostId} />
+				<PostContent currentPostId={currentPostId} />
 			</div>
 		</div>
 	);

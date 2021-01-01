6453e276b8d21f488a22c1f2bab3556226ba70b5
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-01-01 13:48:05 -0800

    style panel and add panel icons

--- components/sidebar/Panel.tsx
+++ components/sidebar/Panel.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import styles from "../../styles/post/panelContainer.module.css";
 import { useRouter } from "next/router";
+import Link from "next/link";
+import CreateIcon from "../../public/svg/create.svg";
+import TagIcon from "../../public/svg/tag.svg";
+import TrashIcon from "../../public/svg/trash.svg";
+import ListIcon from "../../public/svg/list.svg";
+
 const Panel = () => {
 	const router = useRouter();
 	const createPostRoute = () => {
@@ -8,9 +14,23 @@ const Panel = () => {
 	};
 	return (
 		<div className={styles.container}>
-			<h1>Panel</h1>
+			<div className={styles.panelList}>
+				<button className={styles.panelItem}>
+					<ListIcon height={17} width={17} fill="blue" />
+					<span>All Post</span>
+				</button>
+				<button className={styles.panelItem}>
+					<TagIcon height={17} width={17} fill="orange" />
+					<span>Tags</span>
+				</button>
+				<button className={styles.panelItem}>
+					<TrashIcon height={17} width={17} fill="red" />
+					<span>Trash</span>
+				</button>
+			</div>
 			<button className={styles.createBtn} onClick={createPostRoute}>
-				create
+				<CreateIcon height={15} width={15} fill="white" />
+				<p> create</p>
 			</button>
 		</div>
 	);

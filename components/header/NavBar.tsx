11abdc11a68703e058a59dcfa027c8c25de52db2
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-01-01 10:51:24 -0800

    style navbar

--- components/header/NavBar.tsx
+++ components/header/NavBar.tsx
@@ -2,15 +2,35 @@ import React from "react";
 import styles from "../../styles/navbar/Navbar.module.css";
 import HamburgerIcon from "../../assests/svg/hamburgerIcon.svg";
 import Image from "next/image";
+import Link from "next/link";
 
+import Logo from "../../assests/svg/logo-1.svg";
 const NavBar = () => {
 	return (
 		<nav className={styles.navbar}>
 			<HamburgerIcon
 				height={35}
 				width={35}
+				fill="blue"
 				className={styles.hamburgerIcon}
 			/>
+			<Link href="/">
+				<a className={styles.logo}>
+					<Logo height={35} width={35} />
+					<h3> DevBlog</h3>
+				</a>
+			</Link>
+			<div className={styles.centerNavItems}>
+				<Link href="/">
+					<a>Explore</a>
+				</Link>
+				<Link href="/">
+					<a>Home</a>
+				</Link>
+				<Link href="/">
+					<a>About</a>
+				</Link>
+			</div>
 			<div className={styles.author}>
 				<Image
 					src="/img/avatar.png"

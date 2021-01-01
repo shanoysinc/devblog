942456d1321b04eddc88e6e2a3379b33039790ce
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-01-01 09:43:54 -0800

    remove default code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,10 +2,6 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return { ...initialProps };
-	}
 	render() {
 		return (
 			<Html>

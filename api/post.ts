890aae4c08c2eaea67128a406bb8384bd2a7aafc
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-01-02 20:19:36 -0800

    add delete request

--- api/post.ts
+++ api/post.ts
@@ -4,3 +4,4 @@ import { Post } from "../shared/interfaces/Post";
 const url = "http://localhost:3000/api/blog";
 
 export const createPost = (newPost: Post) => axios.post(url, newPost);
+export const deletePost = (postId) => axios.delete(`${url}/${postId}`);
